913480918b23b298ecd325e7550f997a13923eff
Elliot Kim <elliotkim916@gmail.com>
2018-02-03 13:19:51 -0800

    added comments

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -4,15 +4,20 @@ const expect = require('chai').expect;
 // import fizzBuzzer
 const fizzBuzzer = require('../fizzBuzzer');
 
+// unit tests for our fizzBuzzer function
 describe('fizzBuzzer', function() {
-    describe('should return fizz-buzz, buzz, fizz, or the number inputted', function() {
 
+    // test the normal case
+    it('should return fizz-buzz, buzz, fizz, or the number inputted', function() {
+
+        // range of normal inputs, including numbers that aren't divisible by 5
         const normalCases = [
             {a: 15, expected: 'fizz-buzz'},
             {a: 4, expected: 4},
             {a: 3, expected: 'fizz'}
         ];
 
+        // for each set of inputs, 'fizzBuzzer' should produce the expected value
         normalCases.forEach(function(input) {
             const answer = fizzBuzzer(input.a);
             expect(answer).to.equal(input.expected);
@@ -20,12 +25,14 @@ describe('fizzBuzzer', function() {
     });
 
     it('should raise an error if arg not number', function(){
+        // range of bad inputs where none are numbers
         const badInputs = [
             ['a'],
             ['b'],
             ['c']
         ];
 
+        // prove that an error is raised for bad inputs
         badInputs.forEach(function(input) {
             expect(function() {
                 fizzBuzzer(input[0], input[1])
